a363f9fb3ec4dc9edecfde24c87d228e25193dfa
Kristofer <kristofer@fmckl.se>
2023-11-06 10:19:16 +0100

    Compare our data with final OP aproval list

---
+++ src/final-filter-approved.js
@@ -0,0 +1,63 @@
+import * as fs from "fs";
+import * as path from "path";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const dataDirName = path.join(__dirname, "../data");
+
+const rejectedFile = fs.readFileSync(
+  path.join(__dirname, "../approved-applications.txt"),
+  "utf8"
+);
+const approvedList = rejectedFile.split("\n").map((line) => line.trim());
+
+const ourApplicantAddresses = [];
+
+console.log(
+  "\nFinal filtering and comparing of our data and approved applications"
+);
+console.log(
+  "==================================================================="
+);
+
+console.log("\n\nApplications that should be removed:");
+
+// Get list of all files in the current directory
+fs.readdirSync(dataDirName).forEach((file) => {
+  const filePath = path.join(dataDirName, file);
+
+  // Check if it's a file (and not a directory)
+  if (fs.statSync(filePath).isFile()) {
+    // Read file content
+    const fileContent = fs.readFileSync(filePath, "utf8");
+
+    try {
+      // Parse JSON
+      const parsedJson = JSON.parse(fileContent);
+
+      ourApplicantAddresses.push(parsedJson.applicantAddress);
+
+      if (
+        parsedJson.pwIsFlagged === false &&
+        !approvedList.includes(parsedJson.applicantAddress)
+      ) {
+        console.log(`- ${parsedJson.displayName}`);
+        parsedJson.pwIsFlagged = true;
+        fs.writeFileSync(filePath, JSON.stringify(parsedJson, null, 2));
+      }
+    } catch (e) {
+      console.error(`Failed to process ${file}: ${e.message}`);
+    }
+  }
+});
+
+// console.log("\nApplications that should be added:");
+// approvedList.forEach((approvedAddress) => {
+//   if (!ourApplicantAddresses.includes(approvedAddress)) {
+//     console.log(`- ${approvedAddress}`);
+//   }
+// });
